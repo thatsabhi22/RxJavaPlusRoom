8670f7ee9738646f75a56fb7eb9c93b154363875
abhijeet <redefinedabhi2@gmail.com>
2021-04-14 09:33:00 +0530

    Create StudentRepository.kt
    Respository class created as part of MVVM

---
+++ app/src/main/java/com/theleafapps/pro/rxjavaplusroom/data/repository/StudentRepository.kt
@@ -0,0 +1,15 @@
+package com.theleafapps.pro.rxjavaplusroom.data.repository
+
+import com.theleafapps.pro.rxjavaplusroom.data.local.db.DBService
+import com.theleafapps.pro.rxjavaplusroom.data.local.entity.StudentEntity
+
+class StudentRepository(private val dbService: DBService) {
+
+    fun insert(studentEntity: StudentEntity) = dbService.studentDao().insert(studentEntity)
+
+    fun update(studentEntity: StudentEntity) = dbService.studentDao().update(studentEntity)
+
+    fun delete(studentEntity: StudentEntity) = dbService.studentDao().delete(studentEntity)
+
+    fun getAllStudent() = dbService.studentDao().getAllStudents()
+}
