c4c742a8159308b9cae2b48fc7bb13c703082d42
abhijeet <redefinedabhi2@gmail.com>
2021-04-18 06:01:33 +0530

    dialog student is visible
    - student view dialog is visible by tapping Floating action button
    - Save button removed from dialog

--- app/src/main/java/com/theleafapps/pro/rxjavaplusroom/MainActivity.kt
+++ app/src/main/java/com/theleafapps/pro/rxjavaplusroom/MainActivity.kt
@@ -3,15 +3,24 @@ package com.theleafapps.pro.rxjavaplusroom
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.customview.customView
+import com.afollestad.materialdialogs.customview.getCustomView
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputEditText
 import com.theleafapps.pro.rxjavaplusroom.ui.StudentViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel : StudentViewModel
+    private lateinit var addStudentFab : FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addStudentFab = findViewById(R.id.addStudent)
 
         viewModel = ViewModelProvider(
             this,ViewModelProvider.AndroidViewModelFactory.getInstance(application))
@@ -19,6 +28,10 @@ class MainActivity : AppCompatActivity() {
 
         // observer the live data
         observers()
+
+        addStudentFab.setOnClickListener{
+            showStudentDialog()
+        }
     }
 
     private fun observers(){
@@ -38,5 +51,23 @@ class MainActivity : AppCompatActivity() {
 
         })
 
+    }
+
+    private fun showStudentDialog(){
+        val dialog = MaterialDialog(this)
+            .cornerRadius(8f)
+            .cancelable(false)
+            .customView(R.layout.student_view_dialog)
+
+        val customView = dialog.getCustomView()
+        dialog.positiveButton {
+            val name = customView.findViewById<TextInputEditText>(R.id.studentName)
+            val age = customView.findViewById<TextInputEditText>(R.id.studentAge)
+            val subject = customView.findViewById<TextInputEditText>(R.id.studentSubject)
+        }
+        dialog.negativeButton {
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
